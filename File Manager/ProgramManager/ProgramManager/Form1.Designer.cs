e454522cf5729d2ec8a92cf9ca3cce4bc2a2ab94
Geo3777 <87963537+Geo3777@users.noreply.github.com>
2021-08-28 17:38:29 +0300

    File Manager

---
+++ File Manager/ProgramManager/ProgramManager/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿
+namespace ProgramManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.NewFolder = new System.Windows.Forms.Button();
+            this.Copy = new System.Windows.Forms.Button();
+            this.Paste = new System.Windows.Forms.Button();
+            this.Delete = new System.Windows.Forms.Button();
+            this.Rename = new System.Windows.Forms.Button();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.OpenTxt = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // NewFolder
+            // 
+            this.NewFolder.Location = new System.Drawing.Point(-2, 406);
+            this.NewFolder.Name = "NewFolder";
+            this.NewFolder.Size = new System.Drawing.Size(172, 46);
+            this.NewFolder.TabIndex = 0;
+            this.NewFolder.Text = "New Folder";
+            this.NewFolder.UseVisualStyleBackColor = true;
+            this.NewFolder.Click += new System.EventHandler(this.NewFolder_Click);
+            // 
+            // Copy
+            // 
+            this.Copy.Location = new System.Drawing.Point(167, 406);
+            this.Copy.Name = "Copy";
+            this.Copy.Size = new System.Drawing.Size(172, 46);
+            this.Copy.TabIndex = 1;
+            this.Copy.Text = "Copy";
+            this.Copy.UseVisualStyleBackColor = true;
+            this.Copy.Click += new System.EventHandler(this.Copy_Click);
+            // 
+            // Paste
+            // 
+            this.Paste.Location = new System.Drawing.Point(336, 406);
+            this.Paste.Name = "Paste";
+            this.Paste.Size = new System.Drawing.Size(172, 46);
+            this.Paste.TabIndex = 2;
+            this.Paste.Text = "Paste";
+            this.Paste.UseVisualStyleBackColor = true;
+            this.Paste.Click += new System.EventHandler(this.Paste_Click);
+            // 
+            // Delete
+            // 
+            this.Delete.Location = new System.Drawing.Point(504, 406);
+            this.Delete.Name = "Delete";
+            this.Delete.Size = new System.Drawing.Size(172, 46);
+            this.Delete.TabIndex = 3;
+            this.Delete.Text = "Delete";
+            this.Delete.UseVisualStyleBackColor = true;
+            this.Delete.Click += new System.EventHandler(this.Delete_Click);
+            // 
+            // Rename
+            // 
+            this.Rename.Location = new System.Drawing.Point(673, 406);
+            this.Rename.Name = "Rename";
+            this.Rename.Size = new System.Drawing.Size(172, 46);
+            this.Rename.TabIndex = 4;
+            this.Rename.Text = "Rename";
+            this.Rename.UseVisualStyleBackColor = true;
+            this.Rename.Click += new System.EventHandler(this.Rename_Click);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "11.png");
+            this.imageList1.Images.SetKeyName(1, "14.jpg");
+            this.imageList1.Images.SetKeyName(2, "15.jpg");
+            // 
+            // treeView1
+            // 
+            this.treeView1.BackColor = System.Drawing.SystemColors.Control;
+            this.treeView1.ImageIndex = 0;
+            this.treeView1.ImageList = this.imageList1;
+            this.treeView1.Location = new System.Drawing.Point(-2, 3);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.SelectedImageIndex = 0;
+            this.treeView1.Size = new System.Drawing.Size(847, 406);
+            this.treeView1.TabIndex = 5;
+            // 
+            // textBox1
+            // 
+            this.textBox1.AcceptsReturn = true;
+            this.textBox1.AcceptsTab = true;
+            this.textBox1.Location = new System.Drawing.Point(639, 59);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(101, 23);
+            this.textBox1.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(555, 26);
+            this.label1.MaximumSize = new System.Drawing.Size(280, 100);
+            this.label1.MinimumSize = new System.Drawing.Size(50, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(280, 15);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "A se introduce aici numele dorit pentru redenumire ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(577, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(228, 15);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "inainte de selectarea directorului/fisierului";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(568, 85);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(246, 15);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "A nu se deschide sau folosi directorul selectat";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(602, 100);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(180, 15);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "decat dupa un restart la program";
+            // 
+            // OpenTxt
+            // 
+            this.OpenTxt.Location = new System.Drawing.Point(673, 363);
+            this.OpenTxt.Name = "OpenTxt";
+            this.OpenTxt.Size = new System.Drawing.Size(172, 46);
+            this.OpenTxt.TabIndex = 11;
+            this.OpenTxt.Text = "Open txt";
+            this.OpenTxt.UseVisualStyleBackColor = true;
+            this.OpenTxt.Click += new System.EventHandler(this.OpenTxt_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(844, 450);
+            this.Controls.Add(this.OpenTxt);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.Rename);
+            this.Controls.Add(this.Delete);
+            this.Controls.Add(this.Paste);
+            this.Controls.Add(this.Copy);
+            this.Controls.Add(this.NewFolder);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button NewFolder;
+        private System.Windows.Forms.Button Copy;
+        private System.Windows.Forms.Button Paste;
+        private System.Windows.Forms.Button Delete;
+        private System.Windows.Forms.Button Rename;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button OpenTxt;
+    }
+}
+
---
+++ File Manager/ProgramManager/ProgramManager/Form1.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace ProgramManager
+{
+    public partial class Form1 : Form
+    {
+        public static string pathh;
+        public Form1()
+        {
+            InitializeComponent();
+            UmplereCopac();
+            this.treeView1.NodeMouseClick += new TreeNodeMouseClickEventHandler(this.treeView1_NodeMouseClick);
+        }
+        public static TreeNode nod;
+        private void UmplereCopac()
+        {
+            string[] drives = Environment.GetLogicalDrives();
+            foreach (string folder in drives)
+            {
+                nod = new TreeNode(folder);
+                nod.Tag = folder;
+                nod.ImageIndex = 2;
+                nod.Tag = folder;
+                treeView1.Nodes.Add(nod);
+                nod.Nodes.Add(new TreeNode("?"));
+            }
+            treeView1.BeforeExpand += new TreeViewCancelEventHandler(treeView1_BeforeExpand);
+
+        }
+
+        void treeView1_BeforeExpand(object sender, TreeViewCancelEventArgs e)
+        {
+            if ((e.Node.Nodes.Count == 1) && (e.Node.Nodes[0].Text == "?"))
+            {
+                Obtinere(e.Node);
+            }
+        }
+
+        public static string path;
+        private TreeNode Obtinere(TreeNode nod)
+        {
+            string[] foldere;
+            path = (string)nod.Tag;
+            nod.Nodes.Clear();
+            foldere = System.IO.Directory.GetDirectories(path);
+            for (int i = 0; i < foldere.Length; i++)
+            {
+                TreeNode x = new TreeNode(foldere[i].Substring(foldere[i].LastIndexOf('\\') + 1));
+                x.ImageIndex = 0;
+                x.Tag = foldere[i];
+                nod.Nodes.Add(x);
+                x.Nodes.Add(new TreeNode("?"));
+            }
+
+            string[] fisiere = System.IO.Directory.GetFiles(path);
+            for (int i = 0; i < fisiere.Length; i++)
+            {
+                TreeNode y = new TreeNode(System.IO.Path.GetFileName(fisiere[i]));
+                y.Tag = fisiere[i];
+                y.ImageIndex = 1;
+                nod.Nodes.Add(y);
+            }
+
+            return nod;
+        }
+        public static TreeNode newSelected;
+        void treeView1_NodeMouseClick(object sender,
+    TreeNodeMouseClickEventArgs e)
+        {
+            newSelected = e.Node;
+        }
+
+        private void NewFolder_Click(object sender, EventArgs e)
+        {
+            int j = 0;
+            while (j < 2)
+            {
+                string[] foldere;
+                string path = (string)treeView1.SelectedNode.Tag;
+                foldere = System.IO.Directory.GetDirectories(path);
+                Directory.CreateDirectory(Path.Combine(newSelected.Tag as string, "New Folder"));
+                for (int i = 0; i < foldere.Length; i++)
+                {
+                    TreeNode x = new TreeNode(foldere[i].Substring(foldere[i].LastIndexOf('\\') + 1));
+                    x.ImageIndex = 0;
+                    x.Tag = foldere[i];
+                    treeView1.SelectedNode.Nodes.Add(x);
+                }
+                j++;
+            }
+
+        }
+        public void Delete_Click(object sender, EventArgs e)
+        {
+            path = (string)treeView1.SelectedNode.Tag;
+            if(!System.IO.Directory.Exists(treeView1.SelectedNode.FullPath))
+            {
+                File.Delete(Path.Combine(newSelected.Tag as string));
+                if (treeView1.SelectedNode != null)
+                {
+                    if (treeView1.SelectedNode.Parent == null)
+                    {
+                        treeView1.Nodes.Remove(treeView1.SelectedNode);
+                    }
+                    else
+                    {
+                        treeView1.SelectedNode.Parent.Nodes.Remove(treeView1.SelectedNode);
+                    }
+                }
+            }
+            else
+            {
+                Directory.Delete(Path.Combine(newSelected.Tag as string),true);
+                if (treeView1.SelectedNode != null)
+                {
+                    if (treeView1.SelectedNode.Parent == null)
+                    {
+                        treeView1.Nodes.Remove(treeView1.SelectedNode);
+                    }
+                    else
+                    {
+                        treeView1.SelectedNode.Parent.Nodes.Remove(treeView1.SelectedNode);
+                    }
+                }
+            }
+        }
+        
+        private void Rename_Click(object sender, EventArgs e)
+        {
+            string newName;
+            newName = textBox1.Text.ToString();
+            if (!System.IO.Directory.Exists(treeView1.SelectedNode.FullPath))
+            {
+
+                path = (string)treeView1.SelectedNode.Tag;
+               
+                try
+                {
+                    treeView1.SelectedNode.Text = newName;
+                    path = (string)treeView1.SelectedNode.Tag;
+                    FileInfo fi = new FileInfo(path);
+                    fi.MoveTo(fi.Directory + "\\" + newName);
+                }
+                finally
+                {
+
+                }
+            }
+            else
+            {
+                 newName = textBox1.Text.ToString(); ;
+                try
+                {
+                    treeView1.SelectedNode.Text = newName;
+                    path = (string)treeView1.SelectedNode.Tag;
+                    DirectoryInfo di = new DirectoryInfo(path);
+                    di.MoveTo(di.Parent + "\\" + newName);
+                }
+                finally
+                {
+                   
+                }
+            }
+             
+
+    }
+        public static TreeNode cpnode;
+        private void Copy_Click(object sender, EventArgs e)
+        {
+            cpnode = treeView1.SelectedNode;
+        }
+        private static void DirectoryCopy(string sourceDirName, string destDirName, bool copySubDirs)
+        {
+            DirectoryInfo dir = new DirectoryInfo(sourceDirName);
+            if (!dir.Exists)
+            {
+                throw new DirectoryNotFoundException();
+       
+            }
+            DirectoryInfo[] dirs = dir.GetDirectories(); 
+            Directory.CreateDirectory(destDirName);
+            FileInfo[] files = dir.GetFiles();
+            foreach (FileInfo file in files)
+            {
+                string tempPath = Path.Combine(destDirName, file.Name);
+                file.CopyTo(tempPath, false);
+            }
+            if (copySubDirs)
+            {
+                foreach (DirectoryInfo subdir in dirs)
+                {
+                    string tempPath = Path.Combine(destDirName, subdir.Name);
+                    DirectoryCopy(subdir.FullName, tempPath, copySubDirs);
+                }
+            }
+        }
+
+        private void Paste_Click(object sender, EventArgs e)
+        {
+            if (!System.IO.Directory.Exists(treeView1.SelectedNode.FullPath))
+            {
+                File.Copy(Path.Combine((string)cpnode.Parent.Tag, (string)cpnode.Tag), Path.Combine((string)treeView1.SelectedNode.Tag, (string)cpnode.Tag), true);
+            }
+            else
+            {
+                DirectoryCopy((string)cpnode.Parent.Tag, (string)treeView1.SelectedNode.Tag, true);
+            }
+            TreeNode NodeClone = (TreeNode)cpnode.Clone();
+            treeView1.SelectedNode.Nodes.Add(NodeClone);
+
+        }
+
+        private void OpenTxt_Click(object sender, EventArgs e)
+        {
+            var fileToOpen = (string)treeView1.SelectedNode.Tag;
+            var process = new Process();
+            process.StartInfo = new ProcessStartInfo()
+            {
+                UseShellExecute = true,
+                FileName = fileToOpen
+            };
+
+            process.Start();
+            
+        }
+    }
+}
+
+
